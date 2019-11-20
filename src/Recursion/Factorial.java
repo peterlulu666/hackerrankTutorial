4f5a15f6b30ce3ad18567667ed2faac28d69e54c
vpslinuxinstall <shoparoundwyz@gmail.com>
2019-11-19 20:36:34 -0500

    commit

---
+++ src/Recursion/Factorial.java
@@ -0,0 +1,24 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static int Factorial(int number) {
+        if (number <= 0) {
+            return 1;
+
+        } else {
+            return number * Factorial(number - 1);
+
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter number ");
+        int number = scanner.nextInt();
+        System.out.println(Factorial(number));
+
+
+    }
+}
