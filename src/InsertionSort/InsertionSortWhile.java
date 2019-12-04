a3c5837309230db813ee682f50f9daaf93aa1d83
vpslinuxinstall <shoparoundwyz@gmail.com>
2019-12-04 16:46:03 -0500

    commit

--- src/InsertionSort/InsertionSortWhile.java
+++ src/InsertionSort/InsertionSortWhile.java
@@ -5,15 +5,15 @@ import java.util.Arrays;
 public class InsertionSortWhile {
     public static void insertionSort(int[] arr) {
         for (int boundaryIndex = 1; boundaryIndex < arr.length; ++boundaryIndex) {
-            int key = arr[boundaryIndex];
+            int temp = arr[boundaryIndex];
             int mover = boundaryIndex - 1;
 
             /* Move elements of arr[0..i-1], that are
                greater than key, to one position ahead
                of their current position */
-            while (mover >= 0 && arr[mover] > key) {
+            while (mover >= 0 && arr[mover] > temp) {
                 arr[mover + 1] = arr[mover];
-                arr[mover] = key;
+                arr[mover] = temp;
                 mover = mover - 1;
             }
         }
